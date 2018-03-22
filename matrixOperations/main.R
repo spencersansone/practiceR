a22e57d1abeafad16196d8319a58d264db25557d
Spencer Sansone <spencersemail101@gmail.com>
2018-03-21 23:53:39 -0400

    added MatrixOperations

---
+++ matrixOperations/main.R
@@ -0,0 +1,54 @@
+add <- function(matrix1,matrix2) {
+  m = matrix( c(''), nrow = 3, ncol = 3, byrow = TRUE)
+  
+  for (i in 1:9){
+    m[i] = as.numeric(matrix1[i]) + as.numeric(matrix2[i])
+  }
+  
+  return(m)
+}
+
+subtract <- function(matrix1,matrix2) {
+  m = matrix( c(''), nrow = 3, ncol = 3, byrow = TRUE)
+  
+  for (i in 1:9){
+    m[i] = as.numeric(matrix1[i]) - as.numeric(matrix2[i])
+  }
+  
+  return(m)
+}
+
+mainFunc <- function(matrix1,matrix2) {
+  
+  M1 = matrix( c(''), nrow = 3, ncol = 3, byrow = TRUE)
+  M2 = matrix( c('','','','','',''), nrow = 3, ncol = 3, byrow = TRUE)
+  Mtotal = matrix( c('','','','','',''), nrow = 3, ncol = 3, byrow = TRUE)
+  
+  matrixLength = 1:length(M1)
+  
+  for (i in matrixLength){
+    M1[i] = sample(1:10,1)
+    M2[i] = sample(1:10,1)
+  }
+  
+  while (TRUE) {
+    print("==========")
+    print("(1) Add")
+    print("(2) Subtract")
+    answer <- readline("Choice:")
+    
+    if (answer == 1){
+      print("Result: Add")
+      print("===========")
+      
+      print(add(M1,M2))
+    } else if (answer == 2){
+      print("Result: Subtract")
+      print("===========")
+      
+      print(subtract(M1,M2))
+    }
+    
+  }
+}
+mainFunc()
